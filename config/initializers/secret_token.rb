74ed1bf52d163f3cc2112dba2e8de4c9bf6182c2
Mike Harris <mharris717@gmail.com>
2012-11-12 08:29:27 -0500

    Initial State

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoStuffEveryDay::Application.config.secret_token = '7470a2e99f0fabce3bd59f05223308af6f88472c45496f47c8d4d71da6dcd5afac400ecd9012701b629bb73d7ddd0aee8da52a66f6b172c6e4554e6e72c4b180'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DoStuffEveryDay::Application.config.session_store :cookie_store, key: '_do_stuff_every_day_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DoStuffEveryDay::Application.config.session_store :active_record_store
